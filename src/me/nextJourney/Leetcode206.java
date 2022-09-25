af6453f0928d5769b1f9fb2dfeee7f1146d88438
MasamiYui <yingey2011>
2022-09-25 21:55:46 +0800

    re leetcode206

--- src/me/nextJourney/Leetcode206.java
+++ src/me/nextJourney/Leetcode206.java
@@ -6,8 +6,15 @@ public class Leetcode206 {
 
     public ListNode reverseList(ListNode head) {
 
-
-        return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 
     public static void main(String[] args) {
--- src/me/nextJourney/base/ListNode.java
+++ src/me/nextJourney/base/ListNode.java
@@ -2,8 +2,8 @@ package me.nextJourney.base;
 
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
